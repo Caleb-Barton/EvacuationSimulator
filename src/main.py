6f93a4f7f16fcdcd424f0cc2751ec55343b67f2d
Kollin Murphy <kollin.murphy@gmail.com>
2025-11-19 16:51:12 -0700

    loop fix

--- src/main.py
+++ src/main.py
@@ -10,6 +10,8 @@ def get_rand_char():
 
 # Function to play the prisoner's dilemma game when there is a conflict
 # We've assumed P = 2 (it was mathematically the easiest to implement )
+
+
 def prisoners_dilemma(person_list):
     if len(person_list) < 2:
         raise ValueError(
@@ -44,6 +46,8 @@ def prisoners_dilemma(person_list):
     [person.lose() for person in defector_list if person != winner]
 
 # Replace spawn points with people objects
+
+
 def spawn_people(env, strategy=MovementStrategy.STATIC_FIELD, spawn_percent=1.0):
     spawn_count = int(len(env.spawn_points) * spawn_percent)
     selected_spawns = random.sample(env.spawn_points, spawn_count)
@@ -71,6 +75,7 @@ def move(env):
                     env_copy.escaped_people.append(current_person)
     return env_copy
 
+
 def identify_move_conflicts(env, x, y):
     if not env.is_walkable(x, y):
         return []
@@ -81,8 +86,11 @@ def identify_move_conflicts(env, x, y):
             conflict_people.append(person)
     return conflict_people
 
-# Function to loop through the grid moving people while there are still those who haven't reached the exit
+
 def game_loop(env):
+    """
+    Function to loop through the grid moving people while there are still those who haven't reached the exit
+    """
     iteration = 0
     while any(isinstance(item, Person) for row in env.grid for item in row):
         # Find projected moves for each player
@@ -92,7 +100,7 @@ def game_loop(env):
                     item.findProjectedMove(env)
         # Play the prisoner's dilemma if any two people have the same projected moves
         for y in range(len(env.grid)):
-            for x in range(len(row)):
+            for x in range(len(env.grid[y])):
                 conflict_people = identify_move_conflicts(env, x, y)
                 if len(conflict_people) > 1:
                     prisoners_dilemma(conflict_people)
@@ -115,7 +123,7 @@ def main():
     env1 = Environment("env1")
     spawn_people(env1, strategy, spawn_percent=0.75)
 
-    # print initial environment 
+    # print initial environment
     print(env1)
 
     game_loop(env1)
